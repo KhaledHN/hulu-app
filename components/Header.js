e288634ac25b2509b82898869d358939d50c8493
Khaled Hazem <khaled.hazem2009@gmail.com>
2023-08-29 02:37:09 +0300

    Fixing tailwind config file

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,28 @@
+import Image from "next/image";
+import HeaderItem from "./HeaderItem";
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+
 function Header() {
   return (
-    <div className="">
-      <h1>This is the Header</h1>
-    </div>
+    <header className="">
+      <div>
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+      </div>
+      <Image
+        className=" object-contain "
+        src=" https://links.papareact.com/ua6 "
+        alt=""
+        width={200}
+        height={100}
+      />
+    </header>
   );
 }
 
