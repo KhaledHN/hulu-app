dce03e62bbd51e7daa1b747d11ed2b7e38abbcde
Khaled Hazem <khaled.hazem2009@gmail.com>
2023-08-29 03:03:16 +0300

    Adding Headers components

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Head from "next/head";
-import Header from "../components/Header";
+import Header from "../components/Header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
