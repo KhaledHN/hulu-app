f0afdf3a7b218d0115d2b1c7a1ec526d66d48660
Khaled Hazem <khaled.hazem2009@gmail.com>
2023-08-29 23:51:18 +0300

    Adding server side props

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
-import Image from "next/image";
+import { GetServerSideProps } from "next";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import Header from "../components/Header/Header";
 import Nav from "../components/Nav";
+import Content from "../components/Content";
+import requests from "../utils/requests";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
       <Head>
@@ -17,7 +19,23 @@ export default function Home() {
 
       <Nav />
 
-      {/* Resault */}
+      <Content results={results} />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const { genre } = context.query;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    } `
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
